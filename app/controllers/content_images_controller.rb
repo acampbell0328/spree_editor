94e69b54074abc3072f2b660f07117f93b5ce952
Aaron Campbell <acampbell@wrladv.com>
2011-05-24 10:29:56 -0400

    Added image upload functionality

--- app/controllers/content_images_controller.rb
+++ app/controllers/content_images_controller.rb
@@ -1,10 +1,14 @@
 class ContentImagesController < Spree::BaseController
 
   def create
-    @content_image = ContentImage.new(params[:content_image])
+    if Spree::Editor::Config[:current_editor] == "CKEditor"
+      @content_image = ContentImage.new(:attachment => params[:upload])
+    else
+      #debugger
+      @content_image = ContentImage.new(params[:upload])
+    end
     @content_image.save!
 
     render :text => @content_image.attachment.url
   end
-
 end
